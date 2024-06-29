a0bc761b7c409a047b0b7aa7a3d8ff06934aca30
Pedro22-prog <pedro.jgonzalez73@gmail.com>
2024-06-29 18:01:24 -0400

    Cosas que hay que revisar pero subo codigo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+        <h2>Bienvenido</h2>
+        <p>Aqui en la Unimet nosotros nos dedicamos a realizar la mejor comida italiana</p>
+        <button>Ver Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
